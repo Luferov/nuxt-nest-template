e8041878360cfc370454510be1892a3ccc5f75f8
Luferov Victor <lyferov@yandex.ru>
2023-03-09 12:12:46 +0300

    feat: Update dependencies & add ci/di

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yarn
+cd server && cp .env.example .env && cd ..
+cd client && cp .env.example .env && cd ..
+
+echo 'Input this secret key in server/.env file'
+echo 'SECRET_KEY =' $(node -e "console.log(require('crypto').randomBytes(48).toString('hex'))")
