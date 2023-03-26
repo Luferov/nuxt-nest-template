377839fb0f297957e62222c0f24e0ef2188f161c
Luferov Victor <lyferov@yandex.ru>
2023-03-26 19:01:38 +0300

    fix: CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
     credentials: true,
   })
 
